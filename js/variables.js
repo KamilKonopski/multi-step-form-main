32e575d6a436ebf3bd6cd1c8e59f3a8a4fd8b960
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-24 22:26:16 +0100

    add variables

---
+++ js/variables.js
@@ -0,0 +1,8 @@
+export const pages = document.querySelector(".pages");
+export const changePageContainer = document.querySelector(".change-page");
+export const nextPageButton = document.querySelector(".page__next-btn");
+export const backPageButton = document.querySelector(".page__back-btn");
+export const stepNumbers = document.querySelectorAll(".menu__number");
+export const planOptions = document.querySelectorAll(".plan__option");
+export const pickInputs = document.querySelectorAll(".pick__input");
+//# sourceMappingURL=variables.js.map
---
+++ ts/variables.ts
@@ -0,0 +1,15 @@
+export const pages = document.querySelector(".pages") as HTMLDivElement;
+export const changePageContainer = document.querySelector(
+	".change-page"
+) as HTMLDivElement;
+export const nextPageButton = document.querySelector(
+	".page__next-btn"
+) as HTMLButtonElement;
+export const backPageButton = document.querySelector(
+	".page__back-btn"
+) as HTMLButtonElement;
+export const stepNumbers = document.querySelectorAll(".menu__number");
+export const planOptions =
+	document.querySelectorAll<HTMLDivElement>(".plan__option");
+export const pickInputs =
+	document.querySelectorAll<HTMLInputElement>(".pick__input");
