efe34d7386ca55d14382aab62af797eb88bc34bf
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-26 02:39:35 +0100

    comments other not use functions

--- js/app.js
+++ js/app.js
@@ -1,35 +1,6 @@
-import { pages, planOptions, pickInputs, nextPageButton, backPageButton, } from "./variables.js";
+import { pages, nextPageButton, backPageButton } from "./variables.js";
 import { personalSectionElementBuilder } from "./dom-utils.js";
 import { nextPageHandler, backPageHandler } from "./event-functions.js";
-let pickSelectedArray = [];
-for (let i = 0; i < planOptions.length; i++) {
-    planOptions[i].addEventListener("click", function () {
-        let current = document.getElementsByClassName("plan__option--selected");
-        current[0].className = current[0].className.replace(" plan__option--selected", "");
-        this.className += " plan__option--selected";
-    });
-}
-for (let i = 0; i < pickInputs.length; i++) {
-    pickInputs[i].addEventListener("change", function () {
-        const pickOption = pickInputs[i].parentElement;
-        if (this.checked) {
-            pickOption === null || pickOption === void 0 ? void 0 : pickOption.classList.add("pick__option--selected");
-            const pickSelected = pickOption === null || pickOption === void 0 ? void 0 : pickOption.children[1];
-            const pickPriceSelected = pickOption === null || pickOption === void 0 ? void 0 : pickOption.children[3];
-            const pickSelectedObject = {
-                text: pickSelected.innerText,
-                price: pickPriceSelected.innerText,
-            };
-            pickSelectedArray.push(pickSelectedObject);
-        }
-        else {
-            pickOption === null || pickOption === void 0 ? void 0 : pickOption.classList.remove("pick__option--selected");
-            const pickSelected = pickOption === null || pickOption === void 0 ? void 0 : pickOption.children[1];
-            const newPickArray = pickSelectedArray.filter((el) => el.text !== pickSelected.innerText);
-            pickSelectedArray = newPickArray;
-        }
-    });
-}
 document.querySelectorAll(".personal__input").forEach((input) => {
     input.addEventListener("focus", (e) => {
         const target = e.target;
--- ts/app.ts
+++ ts/app.ts
@@ -1,20 +1,14 @@
-import {
-	pages,
-	planOptions,
-	pickInputs,
-	nextPageButton,
-	backPageButton,
-} from "./variables.js";
+import { pages, nextPageButton, backPageButton } from "./variables.js";
 
 import { personalSectionElementBuilder } from "./dom-utils.js";
 
 import { nextPageHandler, backPageHandler } from "./event-functions.js";
 
 // let planOption: string;
-let pickSelectedArray: {
-	text: string;
-	price: string;
-}[] = [];
+// let pickSelectedArray: {
+// 	text: string;
+// 	price: string;
+// }[] = [];
 
 // PERSONAL FORM VALIDATE
 
@@ -26,62 +20,51 @@ let pickSelectedArray: {
 // }
 
 // PLAN SELECTED
-for (let i = 0; i < planOptions.length; i++) {
-	planOptions[i].addEventListener("click", function () {
-		// planOption = planOptions[i].dataset.option!;
-		// console.log(planOption);
-		// document.querySelector<HTMLSpanElement>(".finish__plan-chosen")!.innerText =
-		// 	planOption;
+// for (let i = 0; i < planOptions.length; i++) {
+// 	planOptions[i].addEventListener("click", function () {
+// 		// planOption = planOptions[i].dataset.option!;
+// 		// console.log(planOption);
+// 		// document.querySelector<HTMLSpanElement>(".finish__plan-chosen")!.innerText =
+// 		// 	planOption;
 
-		// const planPrice = planOptions[i].children[2] as HTMLSpanElement;
+// 		// const planPrice = planOptions[i].children[2] as HTMLSpanElement;
 
-		// document.querySelector<HTMLSpanElement>(".finish__plan-price")!.innerText =
-		// 	planPrice.innerText;
+// 		// document.querySelector<HTMLSpanElement>(".finish__plan-price")!.innerText =
+// 		// 	planPrice.innerText;
 
-		let current = document.getElementsByClassName("plan__option--selected");
-		current[0].className = current[0].className.replace(
-			" plan__option--selected",
-			""
-		);
-		this.className += " plan__option--selected";
-	});
-}
-
-// PLAN SWITCH
-// document
-// 	.querySelector<HTMLButtonElement>(".plan__switch")!
-// 	.addEventListener("click", () => {
-// 		console.log("i`m here");
-
-// 		document
-// 			.querySelector<HTMLDivElement>(".plan__switch-dot")!
-// 			.classList.toggle("yearly");
+// 		let current = document.getElementsByClassName("plan__option--selected");
+// 		current[0].className = current[0].className.replace(
+// 			" plan__option--selected",
+// 			""
+// 		);
+// 		this.className += " plan__option--selected";
 // 	});
+// }
 
 // PICK SELECTED
-for (let i = 0; i < pickInputs.length; i++) {
-	pickInputs[i].addEventListener("change", function () {
-		const pickOption = pickInputs[i].parentElement;
+// for (let i = 0; i < pickInputs.length; i++) {
+// 	pickInputs[i].addEventListener("change", function () {
+// 		const pickOption = pickInputs[i].parentElement;
 
-		if (this.checked) {
-			pickOption?.classList.add("pick__option--selected");
-			const pickSelected = pickOption?.children[1] as HTMLLabelElement;
-			const pickPriceSelected = pickOption?.children[3] as HTMLLabelElement;
-			const pickSelectedObject = {
-				text: pickSelected.innerText,
-				price: pickPriceSelected.innerText,
-			};
-			pickSelectedArray.push(pickSelectedObject);
-		} else {
-			pickOption?.classList.remove("pick__option--selected");
-			const pickSelected = pickOption?.children[1] as HTMLLabelElement;
-			const newPickArray = pickSelectedArray.filter(
-				(el) => el.text !== pickSelected.innerText
-			);
-			pickSelectedArray = newPickArray;
-		}
-	});
-}
+// 		if (this.checked) {
+// 			pickOption?.classList.add("pick__option--selected");
+// 			const pickSelected = pickOption?.children[1] as HTMLLabelElement;
+// 			const pickPriceSelected = pickOption?.children[3] as HTMLLabelElement;
+// 			const pickSelectedObject = {
+// 				text: pickSelected.innerText,
+// 				price: pickPriceSelected.innerText,
+// 			};
+// 			pickSelectedArray.push(pickSelectedObject);
+// 		} else {
+// 			pickOption?.classList.remove("pick__option--selected");
+// 			const pickSelected = pickOption?.children[1] as HTMLLabelElement;
+// 			const newPickArray = pickSelectedArray.filter(
+// 				(el) => el.text !== pickSelected.innerText
+// 			);
+// 			pickSelectedArray = newPickArray;
+// 		}
+// 	});
+// }
 
 document.querySelectorAll(".personal__input").forEach((input) => {
 	input.addEventListener("focus", (e) => {
